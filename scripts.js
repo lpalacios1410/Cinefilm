fe7987a69f82d8e4ed90a8da8e9d331f6ef2d54f
Luis Palacios <luisarmando20092009@gmail.com>
2024-12-03 08:57:38 -0400

    [add]create card-layout

--- scripts.js
+++ scripts.js
@@ -16,4 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
             content.style.maxHeight = content.scrollHeight + "px";
             } 
         })};
+        
+        const stars = document.querySelectorAll('.star');
+        
+        stars.forEach(function(star, index) {
+            star.addEventListener('click', function(){
+                for (let i=0; i <= index; i++){
+                    stars[i].classList.add('checked');
+                }
+                for (let i=index+1; i<stars.length; i++){
+                    stars[i].classList.remove('checked');
+                }
+            })
+        })
 })
