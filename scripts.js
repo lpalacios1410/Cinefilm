67b56439f628218b8547521a27bc8f8f78f61985
Luis Palacios <luisarmando20092009@gmail.com>
2024-12-04 10:05:17 -0400

    [add]Responsive general

--- scripts.js
+++ scripts.js
@@ -29,4 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             })
         })
-})
+    })
+    
+    function openNav(){
+        document.getElementById('mobile-menu').style.width = '100%'
+    }
+    
+    function closeNav(){
+        document.getElementById('mobile-menu').style.width = '0%'
+    }
